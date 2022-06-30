01402d848656afa86828f41c4aff913bee3ba15b
NonsoGodson <onyedumekwunonso.com>
2022-06-30 07:21:14 -0700

    Task 0-8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,14 @@
-#!/bin/bash
-{ #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(void) { write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
 
-return (1);
-} }
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 
 int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
