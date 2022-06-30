b4c2bfce8236315880d97b849f063b1705c9f8fa
NonsoGodson <onyedumekwunonso.com>
2022-06-30 11:26:06 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
-int main(void) { write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
-
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 1 (success)
+ */
+int main(void) 
+{
+  write (STDOUT_FILENO, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
 
