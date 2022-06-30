bbc810ad95eb30ae2b7355cbd70928013fc56269
NonsoGodson <onyedumekwunonso.com>
2022-06-30 11:29:10 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,6 @@
  */
 int main(void) 
 {
-  write (STDOUT_FILENO, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
+write (STDOUT_FILENO, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
-
