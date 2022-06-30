80dc4c28a0981a698e9ed447ac65be56b2b77503
NonsoGodson <onyedumekwunonso.com>
2022-06-30 08:20:19 -0700

    Task All

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-
-int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
+/
+ * main - Entry point
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-
-int main(void) { printf("with proper grammar, but the outcome is a piece of art,\n"); return (0); }
+/
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
