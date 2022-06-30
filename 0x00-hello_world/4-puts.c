ec09db00842df1097ba7bcc6a114cc186861e430
NonsoGodson <onyedumekwunonso.com>
2022-06-30 14:36:17 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - Entry point
- *
+ * Description: prints "\"Programming is like building a multilingual puzzle"*
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
-return(0);
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
 }
