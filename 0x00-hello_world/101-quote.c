3177df68a2a1e3dc5d1388b1ce58dc365f46998d
NonsoGodson <onyedumekwunonso.com>
2022-06-30 07:38:37 -0700

    Task 0-8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-/**
- * main - Entry point
- * Return: Always 1 (Success)
- */
-int main(void)
-{
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+int main(void) { write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+
+return (1);
 }
 
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,3 @@
-{ #include <stdio.h>
+#include <stdio.h>
 
-int main(void) { printf("with proper grammar, but the outcome is a piece of art,\n"); return (0); } }
+int main(void) { printf("with proper grammar, but the outcome is a piece of art,\n"); return (0); }
