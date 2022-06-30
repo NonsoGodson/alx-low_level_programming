f249dcf56637a3e93003b5d952a74c7193a72c8a
NonsoGodson <onyedumekwunonso.com>
2022-06-30 13:17:59 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,6 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);
 } 
