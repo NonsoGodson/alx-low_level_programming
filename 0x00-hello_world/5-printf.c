4bb50502a5d2b7277f6d9bc0d95429a49ea17162
NonsoGodson <onyedumekwunonso.com>
2022-06-30 10:33:26 -0700

    Task on C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-(/
+/**
  * main - Entry point
+ *
  * Return: Always 0 (success)
- */)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
