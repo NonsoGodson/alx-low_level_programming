5dd0eca1d8ddfc3791779cf0a0d84c409c3808eb
NonsoGodson <onyedumekwunonso.com>
2022-06-30 11:09:35 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
- int main(void)
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
 {
 puts("\" Programming is like building a multilingual puzzle");
 return (0);
