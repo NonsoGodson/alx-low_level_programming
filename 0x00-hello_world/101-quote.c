336b5147647a9897a234eb1127a4ca569ee0a2de
NonsoGodson <onyedumekwunonso.com>
2022-06-30 14:46:06 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-#include <stdio.h>
-/**
- * main - Entry point
- * Return: Always 1 (success)
- */
-int main(void) 
-{
-write (STDOUT_FILENO, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+/**                                                                                                                                     
+* main - prints exactly and that piece of art is useful"                                                                                
+* - Dora Korpar, 2015-10-19 .                                                                                                           
+* followed by a new line, to the standard error.                                                                                        
+* Return: Always 0 (success)                                                                                                            
+*/                                                                                                                                      
+int main(void)                                                                                                                          
+{                                                                                                                                       
+write(2, "and that piece of art is usefull\" -Dora Korpar, 2015-10-19\n", 59);                                                          
+return (1);                                                                                                                             
 }
