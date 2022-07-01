83234c155fd2ee34f12341e786b6e719c9944325
NonsoGodson <onyedumekwunonso.com>
2022-07-01 05:06:35 -0700

    Task 0-12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,45 +1,34 @@
 #include <stdio.h>
 
 /**
-  * main - Prints 3 combination of numbers
-  *
-  * Return: Always (Success)
-  */
+ * main - Prints all possible combinations of two two-digit numbers,
+ *        ranging from 0-99, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int c, i, k, j;
+	int num1, num2;
 
-	for (c = 48; c <= 57; c++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (i = 48; i <= 57; i++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			for (k = 48; k <= 57; k++)
-			{
-			for (j = 48; j <= 57; j++)
-				{
-	if (((k + j) > (c + i) &&  k >= c) || c < k)
-					{
-					putchar(c);
-					putchar(i);
-					putchar(' ');
-					putchar(k);
-					putchar(j);	
-		if (c + i + k + j == 227 && c == 57)
-					{
-					break;
-					}
-					else
-					{
-					putchar(',');
-					putchar(' ');
-					}
-					}
-				}
-			}
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
-	putchar('\');
+	putchar('\n');
 
 	return (0);
 }
