604b42c8bf7ee6fb46ffb138b069a02c270f7b26
NonsoGodson <onyedumekwunonso.com>
2022-07-05 08:22:12 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,24 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
  * main - Entry point
+ * Description: It prints Holberton, followed by a new line.
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	char *sh = "Holberton";
+   _putchar('H');
+   _putchar('o');
+   _putchar('l');
+   _putchar('b');
+   _putchar('e');
+   _putchar('r');
+   _putchar('t');
+   _putchar('o');
+   _putchar('n');
+   _putchar('\n');
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+return (0);
+}
