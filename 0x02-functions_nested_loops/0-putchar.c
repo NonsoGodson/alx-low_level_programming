7e360d74c76607b226f945f958fc2ff1b73938b4
NonsoGodson <onyedumekwunonso.com>
2022-07-05 04:48:33 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,22 @@
 #include "holberton.h"
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
+ * 
+ * Return: Always 0 (success)
  */
-int main(void)
+int main (void)
 {
+	char *sh = "Holberton\n";
+	int i;
 	char *sh = "Holberton";
 
+	for (i = 0; i <= 11; i++)
 	while (*sh)
 	{
 		_putchar(*sh);
 		sh++;
 	}
 	_putchar('\n');
-        sh++;
-}
 
+	return (0);
+	}
