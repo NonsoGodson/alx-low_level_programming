f219c2cafd1a2dd9837e0bc931c3baeafede4559
NonsoGodson <onyedumekwunonso.com>
2022-07-05 14:26:38 -0700

    Task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,14 @@
-int _putchar(char k);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-int print_sign(int c);
-int _abs(int a);
-int print_last_digit(int a);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
+void num(int r, int n);
