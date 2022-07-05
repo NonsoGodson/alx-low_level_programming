2a7ff614ff45d0e5b6e83ce7d3619c86e6b1de24
NonsoGodson <onyedumekwunonso.com>
2022-07-05 14:44:03 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
- *  print_alphabet - function to print abc
+ *  print_alphabet - function to print a-z
  *
  * Return: 0
  */
 void print_alphabet(void)
 {
-        char c;
-
-        for (c = 'a'; c <= 'z'; c++)
-        {
-                _putchar(c);
-        }
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
 _putchar('\n');
 }
