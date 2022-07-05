1ffe2efd628169300b6068be12ee7a2a68fbcc88
NonsoGodson <onyedumekwunonso.com>
2022-07-05 08:28:06 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,17 +8,17 @@
  * Return: Always 0 (success)
  */
 int main(void)
-   {
-   _putchar('H');
-   _putchar('o');
-   _putchar('l');
-   _putchar('b');
-   _putchar('e');
-   _putchar('r');
-   _putchar('t');
-   _putchar('o');
-   _putchar('n');
-   _putchar('\n');
+{
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 
 return (0);
 }
