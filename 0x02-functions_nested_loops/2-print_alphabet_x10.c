d72aec97cbdcb9cdd785cbbb00549cf31dd10b6f
NonsoGodson <onyedumekwunonso.com>
2022-07-06 03:05:51 -0700

    Nested Loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,19 @@
+
 #include "main.h"
 /**
- * 
+ * print_alphabet_x10 - function to print abc 10 times.
  * Return: 0
  */
 void print_alphabet_x10(void)
 {
-char c, i;
+char c,i;
 :wq
-for (i = 0; i <= 9; i++)
+for (i=0;i<=9;i++)
 {
-for (c = 'a'; c <= 'z'; c++)
+for (c='a';c<='z';c++)
 {
 _putchar(c);
 }
-_putchar('\n);:
+_putchar('\n');:
 }
 }
