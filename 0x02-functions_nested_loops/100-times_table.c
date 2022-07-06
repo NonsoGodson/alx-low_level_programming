3d876fd3a15ce11abc1145b688bb7cc0adc08bb7
NonsoGodson <onyedumekwunonso.com>
2022-07-06 03:44:51 -0700

    Nested Loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,21 +18,24 @@ k = j * i;
 if (j == 0)
 {
 _putchar(k + '0');
-} else if (k < 10 && j != 0)
+} 
+else if (k < 10 && j != 0)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-} else if (k >= 10 && k < 100)
+} 
+else if (k >= 10 && k < 100)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
-} else if (k >= 100)
+} 
+else if (k >= 100)
 {
 _putchar(',');
 _putchar(' ');
