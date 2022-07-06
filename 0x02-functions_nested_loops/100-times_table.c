40b7290cbfc52894f61dad14372af332580eb22a
NonsoGodson <onyedumekwunonso.com>
2022-07-06 03:56:06 -0700

    Nested Loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,34 +17,34 @@ for (j = 0; j <= n; j++)
 k = j * i;
 if (j == 0)
 {
- _putchar(k + '0');
+_putchar(k + '0');
 } 
 else if (k < 10 && j != 0)
 {
- _putchar(',');
- _putchar(' ');
- _putchar(' ');
- _putchar(' ');
- _putchar(k + '0');
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
 } 
 else if (k >= 10 && k < 100)
 {
- _putchar(',');
- _putchar(' ');
- _putchar(' ');
- _putchar((k / 10) + '0');
- _putchar((k % 10) + '0');
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
 } 
 else if (k >= 100)
 {
- _putchar(',');
- _putchar(' ');
- _putchar((k / 100) + '0');
- _putchar(((k / 10) % 10) + '0');
- _putchar((k % 10) + '0');
+_putchar(',');
+_putchar(' ');
+_putchar((k / 100) + '0');
+_putchar(((k / 10) % 10) + '0');
+_putchar((k % 10) + '0');
 }
 }
- _putchar('\n');
+_putchar('\n');
 }
 }
 }
