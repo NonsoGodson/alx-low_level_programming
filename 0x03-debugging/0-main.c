805a315db48374c131ed75624e08449a248fdee8
NonsoGodson <onyedumekwunonso.com>
2022-07-06 16:49:51 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
 #include "main.h"
+
 /**
  * main - Test function that prints positive or negative integers
  * Return: 0
  */
+
 int main(void)
 {
 int i;
-i=0;
+
+i=98;
 positive_or_negative (i);
+
 return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,9 +4,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
+int largest_number(int , int , int);
+void print_remaining_days(int , int , int);
 int convert_day(int month, int day);
-void print_times_table(int n);
 
-#endif 
+#endif /* MAIN_H */ 
