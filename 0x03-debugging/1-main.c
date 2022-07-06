524ea55143aa83ea12d01d379db602690e3dbe35
NonsoGodson <onyedumekwunonso.com>
2022-07-06 16:22:05 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * main - causes an infinite loop
  * Return: 0
@@ -15,7 +17,7 @@ i = 0;
  * {
  * putchar (i);
  * }
-printf("Infinite loop avoided! \\0\/\n");
+printf("Infinite loop avoided! \\o\/\n");
 
 return (0);
 }
