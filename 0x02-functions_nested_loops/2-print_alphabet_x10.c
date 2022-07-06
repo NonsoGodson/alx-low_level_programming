2d7734a1b419e6ec3bccd892bfea6cc1eb00713a
NonsoGodson <onyedumekwunonso.com>
2022-07-06 03:15:03 -0700

    Nested Loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,14 @@
  */
 void print_alphabet_x10(void)
 {
-char c,i;
+	char c, i;
 :wq
-for (i=0;i<=9;i++)
-{
-for (c='a';c<='z';c++)
-{
-_putchar(c);
-}
-_putchar('\n');:
-}
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');:
+	}
 }
