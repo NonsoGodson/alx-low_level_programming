429e5a8ec1e742e326fc87f7d9d783a0a69e983f
NonsoGodson <onyedumekwunonso.com>
2022-07-06 17:17:47 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Test function that prints positive or negative integers
+ * main - Test function that prints if integer is positive or negative 
  * Return: 0
  */
 
