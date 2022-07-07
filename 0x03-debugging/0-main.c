5438e3d6c9c6b74c5c8076746d300619eddb522f
NonsoGodson <onyedumekwunonso.com>
2022-07-06 22:09:48 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,12 +4,11 @@
 * main - Tests function that prints if integer is positive or negative
 * Return: 0
 */
-
 int main(void)
 {
         int i;
 
-        i=0;
+        i = 0;
         positive_or_negative(i);
 
         return (0);
