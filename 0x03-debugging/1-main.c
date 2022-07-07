4e705a648d8958ed785b01597f1ad44adb6141c7
NonsoGodson <onyedumekwunonso.com>
2022-07-07 03:09:18 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-        int i;
-        
-	printf("Infinite loop incoming :(\n");
+int i;
+       
+printf("Infinite loop incoming :(\n");
 
-	i = 0;
-	/**
-	*while (i < 10)
-	*{
-	*	putchar(i);
-	*}
-	*/
-	printf("Infinite loop avoided! \\o/\n");
+i = 0;
+/**
+*while (i < 10)
+*{
+*	putchar(i);
+*}
+*/
+printf("Infinite loop avoided! \\o/\n");
         
-	return (0);
+return (0);
 }
