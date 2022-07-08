d5b05b00f94098ed6d2f0c6b691c601da8234ad2
NonsoGodson <onyedumekwunonso.com>
2022-07-07 21:58:15 -0700

    More Nested Loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,26 +8,26 @@
 
 void print_triangle(int size)
 {
-	int i = 1, ii;
+int i = 1, ii;
 
-	void print_triangle(int size)
+void print_triangle(int size)
 {
-	int i = 1, ii;
+int i = 1, ii;
 
-	while (i <= size && size > 0)
-	{
-		ii = 0;
-		while (ii < size - i)
-		{
-			_putchar('#');
-			ii++;
-		}
-		ii = 0;
-		while (ii < i)
-		{
-			_putchar('#');
-			ii++;
-		}
+while (i <= size && size > 0)
+{
+ii = 0;
+while (ii < size - i)
+{
+_putchar(' ');
+ii++;
+}
+ii = 0;
+while (ii < i)
+{
+_putchar('#');
+ii++;
+}
 if (i == 1)
-	_putchar('\n');
-	}
+_putchar('\n');
+}
