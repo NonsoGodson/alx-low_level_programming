95c852d2235e1cbb3a77745dff9f5ec31bbcb665
NonsoGodson <onyedumekwunonso.com>
2022-07-07 21:33:11 -0700

    More Nested Loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints 0 - 9 except 2, 3
+ * print_most_numbers - prints 0 - 9 except 2, 4
  * Description: prints
  * Return: void
  */
@@ -9,7 +9,7 @@ void print_most_numbers(void)
 {
 	int a = 0;
 	while (a <= 9)
-	{
+	i{
 	if (a != 2 && a != 4)
 		{
 			_putchar(a + '0');
