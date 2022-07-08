033de69de78d359282c08aee24925f9621c4d3d9
NonsoGodson <onyedumekwunonso.com>
2022-07-07 21:37:49 -0700

    More Nested Loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,14 @@
  */
 void print_most_numbers(void)
 {
-	int a = 0;
-	while (a <= 9)
-	i{
-	if (a != 2 && a != 4)
-		{
-			_putchar(a + '0');
-		}
-		a++;
-	}
-	_putchar('\n');
+int a = 0;
+while (a <= 9)
+{
+if (a != 2 && a != 4)
+{
+_putchar(a + '0');
+}
+a++;
+}
+_putchar('\n');
 }
