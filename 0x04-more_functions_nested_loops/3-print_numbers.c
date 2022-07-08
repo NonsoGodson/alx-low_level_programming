b8d37d7ce754766eb679714066e04386769361cf
NonsoGodson <onyedumekwunonso.com>
2022-07-07 21:26:55 -0700

    More Nested Loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,16 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * print_numbers - print 0 - 9
- *
- * Return: void
- *
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
 void print_numbers(void)
 {
-	char a = 0;
+	int ch;
 
-	while (a <= 9)
+	for (ch = 68; ch < 78; ch++)
 	{
-		_putchar(a + '0');
-		a++;
+		_putchar(ch);
 	}
 	_putchar('\n');
 }
