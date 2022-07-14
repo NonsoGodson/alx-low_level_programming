2ba12054518d53266000448499f40eb126c5755d
NonsoGodson <onyedumekwunonso.com>
2022-07-14 05:54:04 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,8 +7,9 @@
  * Return: return value of dest
  */
 
- char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
+	int i;
 	int j;
 
 	i = 0;
@@ -27,6 +28,5 @@
 	}
 
 	dest[i] = '\0';
-
 	return (dest);
 }
