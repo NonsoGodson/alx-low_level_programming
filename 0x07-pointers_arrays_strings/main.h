c630fe107c4568f2f4df9d3d662f58726a1aed21
NonsoGodson <onyedumekwunonso.com>
2022-07-18 06:17:42 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+
 int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
+void *_memset(void *s, int c, size_t n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
+#endif
