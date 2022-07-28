242be58202b1ebb12b39b3a80c777e0afdf896c8
NonsoGodson <onyedumekwunonso.com>
2022-07-28 13:07:36 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,18 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_FILE
+#define MAIN_FILE
 
-/**
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0B-more_malloc_free directory.
- */
+#include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
 #endif
